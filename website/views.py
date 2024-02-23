93472ef82e0ffb6eec2aac76d4d1b2cc4feac3cd
Apolloscar <ohernandez4869@gmail.com>
2024-02-22 21:02:48 -0800

    Made navbar that takes to different pages

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 # when the root is reached, it will call the proceeding function
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    
+    return render_template("home.html")
