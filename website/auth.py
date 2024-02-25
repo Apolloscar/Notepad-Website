6270a3f11334a7a4c8b5a7e5685a9a4dcffd0dc2
Apolloscar <ohernandez4869@gmail.com>
2024-02-24 18:33:48 -0800

    Has alerts when info not valid for forms

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template, request
+from flask import Blueprint,render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -16,4 +16,26 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    # if request is a post request
+    if request.method =='POST':
+        # get elements of request
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            # email should be at least 4 characters long to be valid
+            flash("Email must be at least 4 characters", category="error")
+        elif len(firstName) <2:
+            
+            flash("Name should be more than 1 character", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match", category="error")
+        elif len(password1) < 7:
+            flash("Password should be at least 7 characters", category="error")
+        else:
+            # add valid user to database
+            flash("Account Created", category="success")
+
     return render_template("sign_up.html")
