223c72d0b37d1b86edafc651c85b3b91670b5d75
Apolloscar <ohernandez4869@gmail.com>
2024-02-24 20:19:50 -0800

    Form gives alerts
    Working on database now

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     # encrypt cookies and session data
     app.config['SECRET_KEY'] = 'apple'
+    # shows where database is located
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite://{DB_NAME}'
+    db.init_app(app)
+
 
     from .views import views
     from .auth import auth
