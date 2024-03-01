020844ff058e4d0d2adbeae914fbeb700a05f8d2
Apolloscar <ohernandez4869@gmail.com>
2024-02-29 19:47:52 -0800

    Working on creating db

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +10,8 @@ def create_app():
     # encrypt cookies and session data
     app.config['SECRET_KEY'] = 'apple'
     # shows where database is located
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite://{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
     db.init_app(app)
 
 
@@ -18,4 +20,10 @@ def create_app():
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+    
     return app
--- website/models.py
+++ website/models.py
@@ -1,5 +1,22 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
+# how notes will be formatted
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data= db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default = func.now())
+    # need to pass id of existing user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# How users will be formatted
 class User(db.Model, UserMixin):
-    id = db.Column()
+    id = db.Column(db.Integer, primary_key=True)
+    # each user must have unique email
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+
+    # will have ID of all the notes for the user
+    notes = db.relationship('Note')
