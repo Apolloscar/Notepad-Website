808deb20655b074bed4fab845d435094335ae549
Apolloscar <ohernandez4869@gmail.com>
2024-03-23 19:04:01 -0700

    User can go into own aaccount

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -14,6 +15,7 @@ def create_app():
 
     db.init_app(app)
 
+    
 
     from .views import views
     from .auth import auth
@@ -25,5 +27,14 @@ def create_app():
     
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    # if not logged in go to login page
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
